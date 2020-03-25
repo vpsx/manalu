c53ddb44d3f427e3496565e84c835c4d639b9271
vpsx <19900057+vpsx@users.noreply.github.com>
2020-03-24 23:28:05 -0500

    WEEEEEEE

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HYAAAAAAAAAAAAAA</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fde648',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
