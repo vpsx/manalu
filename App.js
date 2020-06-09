c411b66ba25f690a3e1ee9e422a4c03972843344
vpsx <19900057+vpsx@users.noreply.github.com>
2020-06-09 02:00:08 -0500

    Ask permission, get location, make cat

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Location from 'expo-location';
+
+function Cat(props) {
+  return (
+    <Text>Hello I am your cat, {props.name}.</Text>
+  );
+}
+
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('No permission to access device location');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  });
+
+  let locationText = 'Waiting for location...';
+  if (errorMsg) {
+    locationText = errorMsg;
+  } else if (location) {
+    locationText = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>HYAAAAAAAAAAAAAA</Text>
+      <Text>{locationText}</Text>
+      <Cat name="Sophocles" />
     </View>
   );
 }
@@ -12,7 +43,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fde648',
+    backgroundColor: '#ffb6ce',
     alignItems: 'center',
     justifyContent: 'center',
   },
