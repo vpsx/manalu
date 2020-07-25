87ac54dfebfaf771a35ccba47b5588c4ed5b3bd4
vpsx <19900057+vpsx@users.noreply.github.com>
2020-07-25 00:57:34 -0500

    Rewrite Beacon lifecycle methods as just Effect on App;
    Refactor location as custom Hook;
    Write a bunch of comments

--- App.js
+++ App.js
@@ -6,71 +6,11 @@ import * as Location from 'expo-location';
 // someday I will maybe understand imports vs requires wtf
 const axios = require('axios');
 
-console.log("I'm going vrazy")
-
-axios.get('http://192.168.1.8:5000/getlocation?name=tadpoles')
-.then(function(response) {
-  console.log("GETLOCATION response:")
-  console.log(response.data);
-})
-.catch(function(error) {
-  console.log(error);
-});
-
-axios.post('http://192.168.1.8:5000/givelocation', {
-  Name: "manalu-test",
-  Timestamp: new Date(),
-  Latitude: 1,
-  Longitude: 1,
-})
-.then(function(response) {
-  console.log("GIVELOCATION response:")
-  console.log(response.data);
-})
-.catch(function(error) {
-  console.log(error);
-});
+console.log("Sanity check check check check");
 
 
-class Beacon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: null,
-    };
-  }
-
-  componentDidMount() {
-    this.timer = setInterval(
-      () => this.updateLocation(),
-      60000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  updateLocation() {
-    console.log("Is this doing stuff?")
-    this.setState({
-      location: null, //TODO
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <Text>Write the location here</Text>
-        <Text>Write something else here</Text>
-      </>
-    );
-  }
-
-}
-
-
-export default function App() {
+function useLocation() {
+  // custom Hook to keep track of own location using expo Location
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -93,10 +33,64 @@ export default function App() {
     locationText = JSON.stringify(location);
   }
 
+  // TODO don't actually want to return a string
+  return <Text>{locationText}</Text>;
+}
+
+
+
+export default function App() {
+  // TODO better names needed esp when have both beacon and uh.. radar
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    function updateLocation() {
+      // Since this fn is needed by my effect, I should declare it inside the effect:
+      // https://reactjs.org/docs/hooks-faq.html#is-it-safe-to-omit-functions-from-the-list-of-dependencies
+      console.log("Do the Axios thing! POST location.");
+
+      axios.post('http://192.168.1.8:5000/givelocation', {
+        Name: "manalu-test",
+        Timestamp: new Date(),
+        Latitude: 1,
+        Longitude: 1,
+      })
+      .then(function(response) {
+        console.log("GIVELOCATION response:")
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
+
+    setTimer(setInterval(
+      () => updateLocation(),
+      //1000,
+      60000,
+    ));
+    return () => {
+      clearInterval(timer);
+    };
+  },
+  // The React docs discuss conditional effect firing only in terms of optimization.
+  // https://reactjs.org/docs/hooks-reference.html#conditionally-firing-an-effect
+  // But since this is an Interval, it can only make sense to not reset it on
+  // each render, right? It is not a matter of optimization.
+  // And I am quite sure it is not the case that you aren't supposed to setInterval
+  // in an Effect hook--Hooks purport to cover all the use cases for classes,
+  // which includes the lifecycle methods, and setInterval is definitely done in those.
+  // https://reactjs.org/docs/hooks-faq.html#do-hooks-cover-all-use-cases-for-classes
+  // For this reason I am suspicious about this.
+  // Anyway, whatever, this empty array basically says run effect only on mount, not upd.
+  // Oh also TODO: When you actually use the beacon location instead of dummy coords,
+  // will this need to change......
+  [],
+  );
+
   return (
     <View style={styles.container}>
-      <Text>{locationText}</Text>
-      <Beacon />
+      {useLocation()}
     </View>
   );
 }
